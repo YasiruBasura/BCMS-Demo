5b0a4a192929c59621567b79703bdd88b2af2c6e
Yasi <113851370+YasiruBasura@users.noreply.github.com>
2024-07-21 02:56:27 +0530

    get done in docControl

--- client/src/pages/risk/DocControl.jsx
+++ client/src/pages/risk/DocControl.jsx
@@ -5,7 +5,7 @@ import { useLocation } from 'react-router-dom'
 
 const DocControl = () => {
 
-    const [version, setVersion] = useState("");
+    const [version, setVersion] = useState([]);
     const location = useLocation();
     const {selectedMainSection} = location.state || {}; 
 
@@ -37,42 +37,30 @@ const DocControl = () => {
                     Document Control
                 </h2>
                 <div className="border mt-5 bg-cyan-50 rounded-2xl mx-5">
-                    <div className="pb-20 pt-20 pr-28 pl-28">
+                    <div className="pb-20 pt-20 pr-24 pl-24">
                         <div className="relative overflow-hidden rounded-2xl">
                             <table className="table table-xs w-full text-center mt-10 border border-[#52B14A] rounded-2xl  ">
                                 <thead className="bg-[#00bbf6] h-10 rounded-2xl">
                                     <tr>
-                                        <th>Version</th>
+                                        {/* <th>Version</th> */}
+                                        <th>Date Approved</th>
+                                        <th>Date Last Reviewed</th>
+                                        
+                                        <th>Date for Next Review</th>
                                         <th>Description</th>
-                                        <th>Date</th>
                                     </tr>
                                 </thead>
                                 <tbody className='bg-[#bbe4ff]'>
-                                    <tr>
-                                        <td>1.0</td>
-                                        <td>Initial Version</td>
-                                        <td>10 Oct 2016</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2.0</td>
-                                        <td>2018 review</td>
-                                        <td>20 May 2018</td>
-                                    </tr>
-                                    <tr>
-                                        <td>3.0</td>
-                                        <td>2019 review</td>
-                                        <td>20 May 2019</td>
-                                    </tr>
-                                    <tr>
-                                        <td>4.0</td>
-                                        <td>2020 review</td>
-                                        <td>21 Aug 2020</td>
-                                    </tr>
-                                    <tr>
-                                        <td>5.0</td>
-                                        <td>2021 review</td>
-                                        <td>. . .</td>
-                                    </tr>
+                                    {version.map((version,index)=>(
+                                        <tr key={index}>
+                                            {/* <td>{version.sectionName}</td> */}
+                                            <td>{version.dateApproved}</td>
+                                            <td>{version.dateLastReviewed}</td>
+                                            <td>{version.dateNextReview}</td>
+                                            <td>{version.description}</td>
+                                        </tr>
+                                    ))}
+                                    
                                 </tbody>
                             </table>
                         </div>
